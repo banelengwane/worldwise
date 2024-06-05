b44482315a29bb5067ab4106d4135a0c2fcec5e6
banelengwane <banelengwane@gmail.com>
2024-06-05 19:04:54 +0200

    Dynamic routes with url params

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -8,6 +8,7 @@ function CountriesList({ cities, isLoading }) {
 
     if(!cities.length) return <Message message="Add your first city by clicking on a city on the map" />
 
+    //selecting unique countries
     const countries  = cities.reduce((arr, city) => 
             {
                 if(!arr.map(el=>el.country).includes(city.country)){
