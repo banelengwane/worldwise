5d688c64fc2523a8f87134e33443a3f705f22ee6
banelengwane <banelengwane@gmail.com>
2024-06-16 18:19:21 +0200

    consuming the CitiesContext

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -2,8 +2,11 @@ import Message from './Message'
 import styles from './CountryList.module.css'
 import Spinner from './Spinner';
 import CountryItem from './CountryItem'
+import { useCities } from '../context/CitiesContext';
 
-function CountriesList({ cities, isLoading }) {
+function CountriesList() {
+    const { cities, isLoading } = useCities();
+    
     if(isLoading) return <Spinner />
 
     if(!cities.length) return <Message message="Add your first city by clicking on a city on the map" />
--- src/context/CitiesContext.jsx
+++ src/context/CitiesContext.jsx
@@ -1,4 +1,4 @@
-const { createContext, useState, useEffect } = require("react");
+import { createContext, useState, useEffect, useContext } from 'react'
 
 const BASE_URL = 'http://localhost:8000'
 
@@ -32,4 +32,10 @@ function CitiesProvider({children}){
   </CitiesContext.Provider>
 }
 
-export {CitiesProvider}
+function useCities(){
+    const context = useContext(CitiesContext);
+    if(context === undefined) throw new Error("Cities Context was used outside the CitiesProvider");
+    return context;
+}
+
+export {CitiesProvider, useCities}
