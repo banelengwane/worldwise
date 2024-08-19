a60edb5b1c6b10bb1b4c7ca94732a32b3058553f
banelengwane <banelengwane@gmail.com>
2024-08-19 13:12:57 +0200

    adding lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
+import { lazy, Suspense } from "react"
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 
-import Product from "./pages/Product"
-import Pricing from "./pages/Pricing"
-import Homepage from "./pages/Homepage"
-import PageNotFound from "./pages/PageNotFound"
-import AppLayout from "./pages/AppLayout"
-import Login from "./pages/Login"
+import { CitiesProvider } from "./context/CitiesContext"
+import { AuthProvider } from "./context/FakeAuthContext"
+import ProtectedRoute from "./pages/ProtectedRoute"
+
 import CityList from "./components/CityList"
 import CountriesList from "./components/CountriesList"
 import City from "./components/City"
 import Form from './components/Form'
-import { CitiesProvider } from "./context/CitiesContext"
-import { AuthProvider } from "./context/FakeAuthContext"
-import ProtectedRoute from "./pages/ProtectedRoute"
+import SpinnerFullPage from "./components/SpinnerFullPage"
+
+// import Product from "./pages/Product"
+// import Pricing from "./pages/Pricing"
+// import Homepage from "./pages/Homepage"
+// import PageNotFound from "./pages/PageNotFound"
+// import AppLayout from "./pages/AppLayout"
+// import Login from "./pages/Login"
+
+const Homepage = lazy(() => import('./pages/Homepage'));
+const Product = lazy(() => import('./pages/Product'));
+const Pricing = lazy(() => import('./pages/Pricing'));
+const PageNotFound = lazy(() => import('./pages/PageNotFound'));
+const AppLayout = lazy(() => import('./pages/AppLayout'));
+const Login = lazy(() => import('./pages/Login'));
 
 function App() {
   return (
     <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
+        <Suspense fallback={<SpinnerFullPage />}>
           <Routes>
             <Route index element={<Homepage />} />
             <Route path="product" element={<Product />} />
@@ -39,6 +51,7 @@ function App() {
             </Route>
             <Route path="*" element={<PageNotFound />} />
           </Routes>
+          </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </AuthProvider>
